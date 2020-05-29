0d8b810f9957bab3c27881028ce420bad7ea84c3
m9 <ymgu0709@gmail.com>
2020-05-29 17:23:56 +0900

    Meta Class 추가

--- shopping/shopping/order/models.py
+++ shopping/shopping/order/models.py
@@ -7,3 +7,8 @@ class Order(models.Model):
     product = models.ForeignKey('product.Prodect', on_delete=models.CASCADE, verbose_name='상품')
     quantity = models.IntegerField(verbose_name='수량')
     register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        data_table = 'm9_order' 
+        verbose_name = '주문'
+        verbose_name_plural = '주문'
--- shopping/shopping/product/models.py
+++ shopping/shopping/product/models.py
@@ -8,3 +8,10 @@ class Product(models.Model):
     description = models.TextField(verbose_name='상품설명') # TextField는 제한이 없음
     stcuk = models.IntegerField(verbose_name='재고')
     register_date = models.DateTimeField(auto_now_add=True, verbose_name='등록날짜')
+
+    class Meta:
+        data_table = 'm9_product'
+        verbose_name = '상품'
+        verbose_name_plural = '상품'
+
+    
