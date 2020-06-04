37de8105db5e6c88e1521ea5749046240385ba35
m9 <ymgu0709@gmail.com>
2020-06-04 22:49:39 +0900

    회원가입

--- shopping/shopping/user/views.py
+++ shopping/shopping/user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 
 # Create your views here.
+
+#루트 경로 
+def index(request):
+    return render(request, 'index.html')
+
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegisterForm
+    success_url = '/' # 등록성공하면 root page로
